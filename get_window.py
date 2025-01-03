205f84043f223faf2d2d50d44eddadc01449ddde
BHanna97 <elizabethhanna@alumni.ubc.ca>
2025-01-03 10:59:33 -0700

    Basic get window functionality

--- get_window.py
+++ get_window.py
@@ -1,5 +1,32 @@
-import pygetwindow as gw
+import win32gui
 import time
 
+
 def get_active_window():
+    return win32gui.GetWindowText(win32gui.GetForegroundWindow())
+
+
+active_time = 0
+start_time = 0
+
+try:
+    while True:
+        active_window = get_active_window()
+        if 'Visual Studio Code' in active_window:
+            if start_time == 0:
+                start_time = time.time()
+        else:
+            if start_time != 0:    
+                active_time += time.time() - start_time
+                start_time = 0
+        
+        time.sleep(1)
+
+
+
+
+except KeyboardInterrupt:
+    if start_time:
+        active_time += time.time() - start_time #account for an ongoing session
     
+    print(f"Time elapsed: {active_time / 60:.2f} minutes")
