49e4539e54a7dc6c64a50535ceddca1ba9c873a2
BHanna97 <elizabethhanna@alumni.ubc.ca>
2025-01-03 12:08:17 -0700

    Initialized db

--- get_window.py
+++ get_window.py
@@ -1,6 +1,12 @@
 import win32gui
 import time
+from datetime import date
+import pymongo
 
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["focustimer"]
+
+active_time_collection = db["activetime"]
 
 def get_active_window():
     return win32gui.GetWindowText(win32gui.GetForegroundWindow())
@@ -19,6 +25,12 @@ try:
             if start_time != 0:    
                 active_time += time.time() - start_time
                 start_time = 0
+                active_time_collection.update_one(
+                    {"window_title": "Visual Studio Code","date": date.today().isoformat()},
+                    {"$inc": {"total_active_time_seconds": active_time}},
+                    upsert=True
+                    )
+                active_time = 0
         
         time.sleep(1)
 
@@ -28,5 +40,10 @@ try:
 except KeyboardInterrupt:
     if start_time:
         active_time += time.time() - start_time #account for an ongoing session
-    
-    print(f"Time elapsed: {active_time / 60:.2f} minutes")
+        active_time_collection.update_one(
+                {"window_title": "Visual Studio Code", "date": date.today().isoformat()},
+                {"$inc": {"total_active_time_seconds": active_time}},
+                upsert=True
+                )
+    total_time_today = active_time_collection.find_one({"date": date.today().isoformat()})["total_active_time_seconds"]
+    print(total_time_today)
